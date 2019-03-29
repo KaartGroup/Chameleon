79f3b4e35e6afa6d3bb5ae827d94f7e64dca8cb8
LiuTwo <timothy.liu@kaartgroup.com>
2019-03-29 13:57:46 -0700

    Added functionality addressing issue #16 and #23 concerning columns 'Action' and 'version'

--- main.py
+++ main.py
@@ -79,12 +79,14 @@ class Worker(QObject):
                 sql += "('http://localhost:8111/load_object?new_layer=true&objects=' || "
                 sql += "substr(ifnull(new.\"@type\",old.\"@type\"),1,1) || ifnull(new.\"@id\",old.\"@id\")) AS url, "
             sql += "ifnull(new.\"@user\",old.\"@user\") AS user, substr(ifnull(new.\"@timestamp\",old.\"@timestamp\"),1,10) AS timestamp, "
+            sql += "ifnull(new.\"@version\",old.\"@version\") AS version, "
             if mode != "highway":
                 sql += "ifnull(new.highway,old.highway) AS highway, "
             if mode != "name":
                 sql += "ifnull(new.name,old.name) AS name, "
             sql += f"ifnull(old.{mode},'') AS old_{mode}, ifnull(new.{mode},'') AS new_{mode} "
             if not self.group_output:
+                sql += ", \"modified\" AS \"action\" "
                 sql += ", NULL AS \"notes\" "
             sql += f"FROM {self.oldFileValue} AS old LEFT OUTER JOIN {self.newFileValue} AS new ON old.\"@id\" = new.\"@id\" "
             sql += f"WHERE old_{mode} NOT LIKE new_{mode} "
@@ -95,12 +97,14 @@ class Worker(QObject):
                 sql += "('http://localhost:8111/load_object?new_layer=true&objects=' || "
                 sql += "substr(new.\"@type\",1,1) || new.\"@id\") AS url, "
             sql += "new.\"@user\" AS user, substr(new.\"@timestamp\",1,10) AS timestamp, "
+            sql += "new.\"@version\" AS version, "
             if mode != "highway":
                 sql += "new.highway AS highway, "
             if mode != "name":
                 sql += "new.name AS name, "
             sql += f"ifnull(old.{mode},'') AS old_{mode}, ifnull(new.{mode},'') AS new_{mode} "
             if not self.group_output:
+                sql += ", \"added\" AS \"action\" "
                 sql += ", NULL AS \"notes\" "
             sql += f"FROM {self.newFileValue} AS new LEFT OUTER JOIN {self.oldFileValue} AS old ON new.\"@id\" = old.\"@id\" "
             sql += f"WHERE old.\"@id\" IS NULL AND length(ifnull(new_{mode},'')) > 0"
