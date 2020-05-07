6d58c0b296f777d509560893e39f9b449f8329bc
Evan Derickson <evan@kaartgroup.com>
2020-05-07 15:18:41 -0700

    Convert unittest to pytest and update

--- test/test_core.py
+++ test/test_core.py
@@ -1,11 +1,11 @@
 """
 Unit tests for core.py file.
 """
-import pickle
-import unittest
+import json
 from pathlib import Path
 
-import pandas as pd
+import pytest
+from pandas.testing import assert_frame_equal
 
 from chameleon.core import (
     ChameleonDataFrame,
@@ -15,69 +15,119 @@ from chameleon.core import (
 )
 
 
-class TestQuery(unittest.TestCase):
-    def setUp(self):
-        """
-        Definition of testing vars for TestBuildQuery class.
-        """
-        self.maxDiff = None
-        self.files = {
-            "old": "test/old.csv",
-            "new": "test/new.csv",
-            "output": "test/output",
-        }
+@pytest.mark.parametrize("mode", ["highway", "ref", "name"])
+@pytest.mark.parametrize("grouping", [False, True])
+def test_query_cdf(mode, grouping, cdf_set):
+    cdf = ChameleonDataFrame(cdf_set.source_data, mode)
+    cdf.query_cdf()
 
-        self.file_name = f"{self.files['output']}_name.csv"
 
-        self.mode = "highway"
+@pytest.mark.parametrize("mode", ["highway", "ref", "name"])
+@pytest.mark.parametrize("grouping", [False, True])
+def test_highway_missing(mode, grouping):
+    cdf_set = ChameleonDataFrameSet(
+        "test/old_nohighway.csv", "test/new_nohighway.csv"
+    )
+    # should not raise exception
+    ChameleonDataFrame(cdf_set.source_data, mode=mode)
 
-    def test_chameleon_dataframe_set(self):
-        self.df_set = ChameleonDataFrameSet(
-            self.files["old"], self.files["new"], use_api=False
+
+@pytest.mark.parametrize("mode", ["oneway:conditional"])
+@pytest.mark.parametrize("grouping", [False, True])
+def test_missing_tag(mode, grouping):
+    cdf_set = ChameleonDataFrameSet("test/old.csv", "test/new.csv")
+    cdf_set.separate_special_dfs()
+    with pytest.raises(KeyError):
+        ChameleonDataFrame(cdf_set.source_data, mode).query_cdf()
+
+
+@pytest.mark.parametrize("mode", ["highway", "ref", "oneway:conditional"])
+@pytest.mark.parametrize("grouping", [False, True])
+def name_missing(mode, grouping):
+    cdf_set = ChameleonDataFrameSet("test/old_noname.csv", "test/new_noname.csv")
+    with pytest.raises(KeyError):
+        ChameleonDataFrame(cdf_set.source_data, mode)
+
+
+@pytest.mark.parametrize(
+    "feature_id,gold_file,gold_dict",
+    [
+        (
+            "w389652224",
+            "test/way389652224.json",
+            {
+                "user_new": "schnelli",
+                "changeset_new": "36375031",
+                "timestamp_new": "2016-01-05T07:51:43Z",
+                "version_new": "1",
+                "action": "dropped",
+            },
+        ),
+        (
+            "w796424739",
+            "test/way796424739.json",
+            {
+                "user_new": "wobness",
+                "timestamp_new": "2020-05-07T15:40:29Z",
+                "version_new": "2",
+                "changeset_new": "84841207",
+            },
+        ),
+    ],
+)
+def test_check_api(feature_id, gold_file, gold_dict, files, requests_mock):
+    ftype, fid = split_id(feature_id)
+    gold_file = Path(f"test/{ftype}{fid}.json")
+    with gold_file.open("r") as f:
+        gold_json = json.load(f)
+    cdfs = ChameleonDataFrameSet(**files, use_api=True)
+    requests_mock.get(
+        f"https://www.openstreetmap.org/api/0.6/{ftype}/{fid}/history.json",
+        json=gold_json,
+    )
+    element_attribs = cdfs.check_feature_on_api(feature_id)
+    assert gold_dict == element_attribs
+
+
+@pytest.mark.parametrize("mode", ["highway"])
+def test_add_cdf_to_set(mode, cdf_set):
+    cdf = ChameleonDataFrame(cdf_set.source_data, mode=mode).query_cdf()
+    cdf_set.add(cdf)
+    assert_frame_equal(cdf_set[cdf.chameleon_mode], cdf)
+
+
+@pytest.mark.parametrize(
+    "mixed,gold",
+    [
+        (
+            [
+                "n1234567",
+                "n8901234",
+                "w5678901",
+                "n23456789",
+                "r0123456",
+                "w7801234",
+            ],
+            {
+                "node": ["1234567", "8901234", "23456789"],
+                "way": ["5678901", "7801234"],
+            },
         )
+    ],
+)
+def test_separate_ids_by_feature_type(mixed, gold):
+    assert separate_ids_by_feature_type(mixed) == gold
 
-    def test_chameleon_dataframe_constructor_ungrouped(self):
-        self.df = ChameleonDataFrame(
-            self.df_set.source_data, mode=self.mode, grouping=False
-        ).query_cdf()
 
-    def test_chameleon_dataframe_constructor_grouped(self):
-        pass
-
-    def test_highway_missing_ungrouped(self):
-        pass
-
-    def test_highway_missing_grouped(self):
-        pass
-
-    def test_missing_tag_ungrouped(self):
-        pass
-
-    def test_missing_tag_grouped(self):
-        pass
-
-    def test_check_api(self):
-        pass
-
-    def test_add_cdf_to_set(self):
-        self.df_set[self.mode] = self.df
-
-    def test_separate_ids_by_feature_type(self):
-        mixed = [
-            "n1234567",
-            "n8901234",
-            "w5678901",
-            "n23456789",
-            "r0123456",
-            "w7801234",
-        ]
-        gold = {
-            "node": ["1234567", "8901234", "23456789"],
-            "way": ["5678901", "7801234"],
-        }
-        self.assertEqual(separate_ids_by_feature_type(mixed), gold)
-
-    def test_split_id(self):
-        fid = "w1234567"
-        gold = ("way", "1234567")
-        self.assertEqual(gold, split_id(fid))
+@pytest.mark.parametrize(
+    "fid,gold",
+    [
+        ("n1234567", ("node", "1234567")),
+        ("w1234567", ("way", "1234567")),
+        ("r1234567", ("relation", "1234567")),
+        ("1234567", (None, "1234567")),
+        (1234567, (None, "1234567")),
+    ],
+)
+def test_split_id(fid, gold):
+    assert gold == split_id(fid)
--- test/test_main.py
+++ test/test_main.py
@@ -1,295 +1,222 @@
 """
 Unit tests for the main.py file
 """
-
-import unittest
 from pathlib import Path
 
-import oyaml as yaml
-
+import pandas as pd
+import pytest
 from PySide2 import QtWidgets
 from PySide2.QtCore import Qt
 from PySide2.QtTest import QTest
 
-import chameleon.main
+from chameleon import main, core
 
-# from pathlib import Path
 # TEST_FOLDER = Path("test")
 
 app = QtWidgets.QApplication(["."])
 
 
-class TestBuildQuery(unittest.TestCase):
+@pytest.fixture
+def worker_files():
+    return {
+        "old": "test/old.csv",
+        "new": "test/new.csv",
+        "output": "test/output",
+    }
+
+
+@pytest.fixture
+def worker(mainapp, worker_files):
+    return main.Worker(mainapp, {"name"}, worker_files)
+
+
+# Worker tests
+
+
+def test_load_extra_columns(worker):
+    gold_columns = {
+        "resolution status": {
+            "validate": "list",
+            "source": [
+                "Fixed",
+                "Not an issue",
+                "Unsure/Too hard - Leaving as is",
+                "On Hold",
+            ],
+        },
+        "editor notes": None,
+        "QC status": {
+            "validate": "list",
+            "source": ["Validated", "Invalidated", "On Hold"],
+        },
+        "QCer": None,
+        "QC notes": None,
+        "reworked": {
+            "validate": "list",
+            "source": ["Addressed", "Disputed", "On Hold"],
+        },
+        "reworked notes": None,
+    }
+    loaded_columns = worker.load_extra_columns()
+    assert gold_columns == loaded_columns
+
+
+def test_history_writer():
+    pass
+
+
+@pytest.mark.parametrize(
+    "newfile,outcome",
+    [("test/new_highdeletions.csv", False), ("test/new.csv", True)],
+)
+def test_high_deletions_checker(worker, newfile, outcome):
+    # TODO Need to simulate click on confirmation dialog
+    cdf_set = core.ChameleonDataFrameSet("test/old.csv", newfile)
+    assert worker.high_deletions_checker(cdf_set) is outcome
+
+
+def test_overwrite_confirm():
+    # TODO Need to simulate click on confirmation dialog
+    pass
+
+
+def test_check_api_deletions(worker, cdf_set):
+    worker.check_api_deletions(cdf_set)
+    cdf_set.separate_special_dfs()
+    assert len(cdf_set["deleted"]["changeset_new"].isna) == 0
+
+
+def test_csv_output(self):
+    pass
+
+
+def test_excel_output(self):
+    pass
+
+
+def test_geojson_output(self):
+    pass
+
+
+# GUI Tests
+
+
+@pytest.fixture
+def favorite_location():
+    return Path("test/test_favorites.yaml")
+
+
+@pytest.fixture
+def mainapp(favorite_location):
+    app = main.MainApp()
+    return app
+
+
+def test_add_to_list(mainapp):
     """
-    Unittesting for Worker class comparing sample csv and code outputs.
-    This test validates sql query and analysis outputs.
+    Verifies 'Add' button function for search bar.
     """
-
-    # def __init__(self):
-    #     super().__init__()
-
-    def setUp(self):
-        """
-        Definition of testing vars for TestBuildQuery class.
-        """
-        self.maxDiff = None
-        self.files = {
-            "old": "test/old.csv",
-            "new": "test/new.csv",
-            "output": "test/output",
-        }
-        self.file_name = f"{self.files['output']}_name.csv"
-        self.func = chameleon.main.Worker("name", self.files, False, None)
-
-    def test_build_query_ungrouped(self):
-        """
-        Comparison of sql query used for ungrouped analysis.
-        """
-        # print(gold_sql)
-        test_sql = self.func.build_query("name", self.files, False, True)
-        # print(test_sql)
-        self.assertEqual(test_sql, self.gold_sql)
-
-    def test_execute_query_ungrouped(self):
-        """
-        Tests that querying an output without grouping gives expected results
-        """
-        test_output = (
-            self.func.execute_query("name", self.files, False, True)
-        ).data
-        with open("test/test_ungrouped_q_output.yaml", "r") as file:
-            gold_ungrouped_output = yaml.load(file)
-        self.assertEqual(test_output, gold_ungrouped_output)
-
-    def test_execute_query_grouped(self):
-        """
-        Tests that querying an output with grouping gives expected results
-        """
-        test_output = (
-            self.func.execute_query("name", self.files, True, True)
-        ).data
-        with open("test/test_grouped_q_output.yaml", "r") as file:
-            gold_grouped_output = yaml.load(file)
-        self.assertEqual(test_output, gold_grouped_output)
-
-    def test_highway_missing_ungrouped(self):
-        """
-        Test that omitting a highway tag in the inputs does not block analysis in ungrouped mode
-        """
-        files = {
-            "old": "test/old_nohighway.csv",
-            "new": "test/new_nohighway.csv",
-            "output": "test/output",
-        }
-        test_output = (
-            self.func.execute_query("name", files, False, False)
-        ).data
-        with open("test/test_ungrouped_q_output_nohighway.yaml", "r") as file:
-            gold_ungrouped_output = yaml.load(file)
-        self.assertEqual(test_output, gold_ungrouped_output)
-
-    def test_highway_missing_grouped(self):
-        """
-        Test that omitting a highway tag in the inputs does not block analysis in grouped mode
-        """
-        files = {
-            "old": "test/old_nohighway.csv",
-            "new": "test/new_nohighway.csv",
-            "output": "test/output",
-        }
-        test_output = (self.func.execute_query("name", files, True, False)).data
-        with open("test/test_grouped_q_output_nohighway.yaml", "r") as file:
-            gold_grouped_output = yaml.load(file)
-        self.assertEqual(test_output, gold_grouped_output)
-
-    def test_missing_tag_ungrouped(self):
-        """
-        Test that a missing tag gives a helpful error message in ungrouped mode
-        """
-        files = {
-            "old": "test/old_noname.csv",
-            "new": "test/new_noname.csv",
-            "output": "test/output",
-        }
-        test_output = (
-            self.func.execute_query("name", files, False, False)
-        ).status
-        self.assertEqual(test_output, "error")
-
-    def test_missing_tag_grouped(self):
-        """
-        Test that a missing tag gives a helpful error message in grouped mode
-        """
-        files = {
-            "old": "test/old_noname.csv",
-            "new": "test/new_noname.csv",
-            "output": "test/output",
-        }
-        test_output = (
-            self.func.execute_query("name", files, True, False)
-        ).status
-        self.assertEqual(test_output, "error")
-
-    # def test_write_file_ungrouped(self):
-    #     """
-    #     Comparison of sample and code csv outputs.
-    #     """
-    #     self.func.write_file(self.gold_sql, self.file_name, "name")
-    #     with open(self.file_name, "r") as file:
-    #         test_file = file.read()
-    #     with open("test/test_name.csv", "r") as file:
-    #         gold_file = file.read()
-    #     self.assertMultiLineEqual(test_file, gold_file)
-
-    def test_csv_output(self):
-        pass
-
-    def test_excel_output(self):
-        pass
-
-    def test_geojson_output(self):
-        pass
+    mainapp.searchBox.insert("highway")
+    QTest.mouseClick(mainapp.searchButton, Qt.LeftButton)
+    assert len(mainapp.listWidget.findItems("highway", Qt.MatchExactly)) > 0
+    assert mainapp.searchBox.text is not True
 
 
-class TestGUI(unittest.TestCase):
+def test_add_special_char_to_list(mainapp):
     """
-    Unittest for MainApp class validating UI functionalities.
-    This test verifies button function, user input values and backend
-    UI display parameters for the application.
+    Verifies 'Add' button function for user input with
+    special characters.
     """
-
-    def setUp(self):
-        """
-        Definition of testing vars for TestGUI class.
-        """
-        self.mainapp = chameleon.main.MainApp()
-        self.favorite_location = Path("test/test_favorites.yaml")
-        self.mainapp.fav_btn_populate(self.favorite_location)
-
-    def test_add_to_list(self):
-        """
-        Verifies 'Add' button function for search bar.
-        """
-        self.mainapp.searchBox.insert("highway")
-        QTest.mouseClick(self.mainapp.searchButton, Qt.LeftButton)
-        self.assertGreater(
-            len(self.mainapp.listWidget.findItems("highway", Qt.MatchExactly)),
-            0,
-        )
-        self.assertIsNot(self.mainapp.searchBox.text, True)
-
-    def test_add_special_char_to_list(self):
-        """
-        Verifies 'Add' button function for user input with
-        special characters.
-        """
-        self.mainapp.searchBox.insert("addr:housenumber")
-        QTest.mouseClick(self.mainapp.searchButton, Qt.LeftButton)
-        self.assertGreater(
-            len(
-                self.mainapp.listWidget.findItems(
-                    "addr:housenumber", Qt.MatchExactly
-                )
-            ),
-            0,
-        )
-
-    def test_add_spaces_to_list(self):
-        """
-        Verifies space and empty values are ignored by
-        the 'Add' function.
-        """
-        self.mainapp.listWidget.clear()
-        self.mainapp.searchBox.insert("   ")
-        QTest.mouseClick(self.mainapp.searchButton, Qt.LeftButton)
-        self.assertEqual(self.mainapp.listWidget.count(), 0)
-
-    def test_remove_from_list(self):
-        """
-        Verifies 'Delete' button function for QListWidget.
-        """
-        self.mainapp.listWidget.addItems(["name", "highway", "ref"])
-        # nameitem = QtWidgets.QListWidgetItem("name")
-        nameitems = self.mainapp.listWidget.findItems("name", Qt.MatchExactly)
-        for i in nameitems:
-            self.mainapp.listWidget.setCurrentItem(i)
-            # self.mainapp.listWidget.setCurrentRow(0)
-            # self.mainapp.listWidget.item(
-            #     current_list.index("name")).setSelected(True)
-            QTest.mouseClick(self.mainapp.deleteItemButton, Qt.LeftButton)
-            # self.mainapp.delete_tag()
-        self.assertEqual(
-            len(self.mainapp.listWidget.findItems("name", Qt.MatchExactly)), 0
-        )
-        self.assertGreater(
-            len(self.mainapp.listWidget.findItems("highway", Qt.MatchExactly)),
-            0,
-        )
-
-    def test_clear_list_widget(self):
-        """
-        Verifies 'Clear' button function for wiping items
-        in QListWidget.
-        """
-        self.mainapp.listWidget.addItems(
-            [
-                "name",
-                "highway",
-                "ref",
-                "building",
-                "addr:housenumber",
-                "addr:street",
-            ]
-        )
-        QTest.mouseClick(self.mainapp.clearListButton, Qt.LeftButton)
-        self.assertEqual(self.mainapp.listWidget.count(), 0)
-
-    def test_fav_btn_populate(self):
-        """
-        Verifies all favorite buttons are populated with
-        the correct default and history values.
-        """
-        self.assertEqual(self.mainapp.popTag1.text(), "name")
-        self.assertEqual(self.mainapp.popTag2.text(), "highway")
-        self.assertEqual(self.mainapp.popTag3.text(), "addr:place")
-        self.assertEqual(self.mainapp.popTag4.text(), "ref")
-        self.assertEqual(self.mainapp.popTag5.text(), "addr:housenumber")
-
-    def test_fav_btn_click(self):
-        """
-        Verifies favorite button function and reception
-        of favorite values by the QListWidget.
-        """
-        self.assertEqual(self.mainapp.listWidget.count(), 0)
-        self.assertEqual(self.mainapp.popTag1.text(), "name")
-        print("Clicking fav button 1")
-        QTest.mouseClick(self.mainapp.popTag1, Qt.LeftButton)
-        self.assertGreater(
-            len(self.mainapp.listWidget.findItems("name", Qt.MatchExactly)), 0
-        )
-
-    def test_autocompleter(self):
-        """
-        Checks that autocompleter tags get loaded without raising an exception.
-        """
-        self.mainapp.auto_completer()
-
-    # def test_expand_user(self):
-    #     self.mainapp.newFileNameBox.insert('~/Desktop/')
-    #     self.mainapp.newFileNameBox.clearFocus()
-    #     self.assertEqual(self.newFileNameBox)
-
-    def test_no_settings_files(self):
-        """
-        Test running chameleon without existing counter.yaml/settings.yaml
-        """
-        pass
+    mainapp.searchBox.insert("addr:housenumber")
+    QTest.mouseClick(mainapp.searchButton, Qt.LeftButton)
+    assert (
+        len(mainapp.listWidget.findItems("addr:housenumber", Qt.MatchExactly))
+        > 0
+    )
 
 
-# class TestFavBtns(unittest.TestCase):
-#     def setUp(self):
+def test_add_spaces_to_list(mainapp):
+    """
+    Verifies space and empty values are ignored by
+    the 'Add' function.
+    """
+    mainapp.listWidget.clear()
+    mainapp.searchBox.insert("   ")
+    QTest.mouseClick(mainapp.searchButton, Qt.LeftButton)
+    assert mainapp.listWidget.count() == 0
 
-#     def test_fav_btns(self):
 
-if __name__ == "__main__":
-    unittest.main()
+def test_remove_from_list(mainapp):
+    """
+    Verifies 'Delete' button function for QListWidget.
+    """
+    mainapp.listWidget.addItems(["name", "highway", "ref"])
+    nameitems = mainapp.listWidget.findItems("name", Qt.MatchExactly)
+    for i in nameitems:
+        mainapp.listWidget.setCurrentItem(i)
+        QTest.mouseClick(mainapp.deleteItemButton, Qt.LeftButton)
+    assert len(mainapp.listWidget.findItems("name", Qt.MatchExactly)) == 0
+    assert len(mainapp.listWidget.findItems("highway", Qt.MatchExactly)) > 0
+
+
+def test_clear_list_widget(mainapp):
+    """
+    Verifies 'Clear' button function for wiping items
+    in QListWidget.
+    """
+    mainapp.listWidget.addItems(
+        [
+            "name",
+            "highway",
+            "ref",
+            "building",
+            "addr:housenumber",
+            "addr:street",
+        ]
+    )
+    QTest.mouseClick(mainapp.clearListButton, Qt.LeftButton)
+    assert mainapp.listWidget.count() == 0
+
+
+def test_fav_btn_populate(mainapp, favorite_location):
+    """
+    Verifies all favorite buttons are populated with
+    the correct default and history values.
+    """
+    mainapp.fav_btn_populate(favorite_location)
+    assert mainapp.popTag1.text() == "name"
+    assert mainapp.popTag2.text() == "highway"
+    assert mainapp.popTag3.text() == "addr:place"
+    assert mainapp.popTag4.text() == "ref"
+    assert mainapp.popTag5.text() == "addr:housenumber"
+
+
+def test_fav_btn_click(mainapp):
+    """
+    Verifies favorite button function and reception
+    of favorite values by the QListWidget.
+    """
+    assert mainapp.listWidget.count() == 0
+    assert mainapp.popTag1.text() == "name"
+    QTest.mouseClick(mainapp.popTag1, Qt.LeftButton)
+    assert len(mainapp.listWidget.findItems("name", Qt.MatchExactly)) > 0
+
+
+def test_autocompleter(mainapp):
+    """
+    Checks that autocompleter tags get loaded without raising an exception.
+    """
+    mainapp.auto_completer()
+
+
+def test_expand_user(mainapp):
+    mainapp.newFileNameBox.insert("~/Desktop/")
+    mainapp.newFileNameBox.clearFocus()
+    assert mainapp.newFileNameBox.text() == str(Path.home() / "Desktop")
+
+
+def test_no_settings_files(mainapp):
+    """
+    Test running chameleon without existing counter.yaml/settings.yaml
+    """
+    pass
